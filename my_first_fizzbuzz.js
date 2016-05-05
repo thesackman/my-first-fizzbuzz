1260224dc3ea863e0cf99ee6a31f6fd9b36017fe
thesackman <xbtompkinson@gmail.com>
2016-05-04 20:01:27 -0500

    Added files via upload

---
+++ my_first_fizzbuzz.js
@@ -0,0 +1,20 @@
+for (i = 1; i <= 20; i++) {
+  
+  var fb = '';
+  
+    if (i % 3 === 0){
+        fb += 'Fizz';
+    }
+
+    if (i % 5 === 0){
+        fb += 'Buzz';
+    }
+
+    if (fb != ''){
+        console.log(fb);
+    }
+
+    else {
+        console.log(i);
+    }
+}
